2949ecd1f83ea50d70793f7f77862ef5007f70ae
hirCodd <hefengen@hotmail.com>
2020-01-10 23:47:57 +0800

    init project

---
+++ src/main/java/com/javanorth/javanorthapp/JavanorthappApplication.java
@@ -0,0 +1,13 @@
+package com.javanorth.javanorthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanorthappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavanorthappApplication.class, args);
+    }
+
+}
